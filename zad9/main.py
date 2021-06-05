2129a055906c180dc70b225817149a212746c774
RahimFatih <65657905+RahimFatih@users.noreply.github.com>
2021-06-05 10:39:12 +0200

    Poprawa

--- zad9/main.py
+++ zad9/main.py
@@ -1,5 +1,4 @@
 import socket
-import smtplib, ssl
 
 def web_page():
     if deviceStan == "ON":
@@ -11,9 +10,9 @@ def web_page():
   <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
   h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
   border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2{background-color: #4286f4;}</style></head><body> <h1>Kochasz mnie Olu?</h1> 
-  <p><a href="/?led=on"><button class="button">TAK!</button></a></p>
-  <p><a href="/?led=off"><button class="button button2">Jeszcze raz!</button></a></p></body></html>"""
+  .button2{background-color: #4286f4;}</style></head><body> <h1>Serwer WWW Denis Firat 249031</h1> 
+  <p>Stan urzadzenia: <strong>""" + gpio_state + b"""</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
+  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
     return html
 
 
@@ -21,37 +20,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
 
-
-smtp_server = "smtp.gmail.com"
-port = 587  # For starttls
-sender_email = "testowydenisa@gmail.com"
-password = "@Dnamama1google@"
-
-# Create a secure SSL context
-context = ssl.create_default_context()
-
-# Try to log in to server and send email
-try:
-    server = smtplib.SMTP(smtp_server,port)
-    server.ehlo() # Can be omitted
-    server.starttls(context=context) # Secure the connection
-    server.ehlo() # Can be omitted
-    server.login(sender_email, password)
-    # TODO: Send email here
-except Exception as e:
-    # Print any error messages to stdout
-    print(e)
-finally:
-    server.quit() 
-
-message = """\
-Subject: Wiadomosc dla Oli
-
-Ja Ciebie tez kocham."""
-receiver_email = "aleksandra.wygnaniec@gmail.com"
-#receiver_email = "dfirat998@gmail.com"
-sended=0
-deviceStan = "OFF"
 while True:
     conn, addr = s.accept()
     print('Got a connection from %s' % str(addr))
@@ -60,21 +28,13 @@ while True:
     print('Content = %s' % request)
     led_on = request.find('/?led=on')
     led_off = request.find('/?led=off')
-    if led_on == 6 and sended==0:
+    deviceStan="OFF"
+    if led_on == 6:
         print('LED ON')
         deviceStan = "ON"
-        context = ssl.create_default_context()
-        with smtplib.SMTP(smtp_server, port) as server:
-            server.ehlo()  # Can be omitted
-            server.starttls(context=context)
-            server.ehlo()  # Can be omitted
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
-        sended=1
     if led_off == 6:
         print('LED OFF')
         deviceStan = "OFF"
-        sended=0
     response = web_page()
     conn.send(b'HTTP/1.1 200 OK\n')
     conn.send(b'Content-Type: text/html\n')
